8490d652afff263c34fbba3846a86a66909854ea
Ciahaka <wilddiver.by@gmail.com>
2022-11-25 21:47:12 +0300

    change Index for bubble

--- src/05_Sort_Victor/main.js
+++ src/05_Sort_Victor/main.js
@@ -17,8 +17,13 @@ const compareFn = (a, b) => {
     }
     return 25
 }
+const shortCompareFn = (a, b) => a - b
+const shortCompareFnReverse = (a, b) => b - a
+
 console.log(numbers.sort(compareFn))
 console.log(numbers.sort(compareFn).reverse())
+console.log(numbers.sort(shortCompareFn))
+console.log(numbers.sort(shortCompareFnReverse))
 
 
 const students = [
@@ -62,8 +67,13 @@ const students = [
 //a и b - объекты
 // console.log(students.sort((a,b)=>b.scores - a.scores))
 // console.log(students.sort((a,b)=>a.name.localeCompare(b.name)))
-//вместо localeCompare
+//вместо
 console.log(students.sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1))
+const sortStudents = students.sort((a, b) => a.scores - b.scores)
+const sortStudents_1 = students.sort((a, b) => b.scores - a.scores)
+console.log(sortStudents)
+console.log(sortStudents_1)
+
 
 //bubble sort
 const nums = [54, 43, 28, 90, 67, 1, 19]
@@ -74,12 +84,12 @@ for (let j = 0; j < nums.length - 1; j++) {      //цикл в цикле, чт�
         if (nums[i] > [i + 1]) {
             isSorted = false;   //при такой записи не забыть про ; .
             // const temp = nums[i]   //-пустой стакан
-            // nums[i + 1] = nums[i + 1]
+            // nums[1] = nums[i + 1]
             // nums[i + 1] = temp
             [nums[i + 1], nums[i]] = [nums[i], nums[i + 1]]  //запись короткая Производительность ниже чем у цикла for
         }
     }
     if (isSorted) break
 }
-
+console.log(nums)
 //
